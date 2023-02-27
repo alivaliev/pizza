dfa295c5c747994bca6d62a6317c74db7a666e16
alivaliev <aliveliyev@hotmail.com>
2023-02-27 22:24:08 +0400

    qocu

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let crust = document.querySelector('.crust')
+let dropCrust = document.querySelector('.dropcrust')
+let sauce = document.querySelector('.sauce')
+let ingredients = document.querySelectorAll('.ingredients')
+
+// crust.addEventListener('click', function(){
+//     dropCrust.classList.toggle('active')
+
+// })
+
+// ingredients.forEach(function(item){
+//     let header = item.querySelector('.crust')
+//     let content = item.querySelector('.dropcrust')
+
+//     header.onclick = function(){
+//         content.classList.toggle('active')
+//     }
+// })
+
+for (let i = 0; i < ingredients.length; i++) {
+    const qocu = ingredients[i];
+    qocu.addEventListener('click',function(){
+        
+    })
+    
+}
